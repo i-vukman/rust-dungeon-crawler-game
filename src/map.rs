b0f7a45a8d4bfab89102139e6304f9d698339caf
Ivan Vukman <ivan.vukman@gmail.com>
2023-10-28 18:25:39 +0200

    Render player on map

--- src/map.rs
+++ src/map.rs
@@ -1,4 +1,4 @@
-use bracket_lib::terminal::{to_cp437, BTerm, BLACK, GREEN, YELLOW, Point};
+use bracket_lib::terminal::{to_cp437, BTerm, Point, BLACK, GREEN, YELLOW};
 
 use crate::prelude::{SCREEN_HEIGHT, SCREEN_WIDTH};
 
@@ -50,12 +50,10 @@ impl Map {
     }
 
     pub fn can_enter_tile(&self, point: Point) -> bool {
-        self.in_bounds(point)
-            && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
+        self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
     }
 
     pub fn in_bounds(&self, point: Point) -> bool {
-        point.x >= 0 && point.x < SCREEN_WIDTH
-            && point.y >= 0 && point.y < SCREEN_HEIGHT
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
     }
 }
